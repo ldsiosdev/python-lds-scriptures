4857d10cd428c5674e7b3b0f4ea0a61edb2a857a
bencrowder <ben.crowder@gmail.com>
2014-07-07 14:41:04 -0600

    Added chapter range test

--- scriptures/tests/test_format.py
+++ scriptures/tests/test_format.py
@@ -71,6 +71,7 @@ class TestFormat(unittest.TestCase):
         self.assertEqual(format([ref('/scriptures/ot/gen/39'), ref('/scriptures/ot/gen/40')]), u'Genesis 39–40')
         self.assertEqual(format([ref('/scriptures/ot/gen/39'), ref('/scriptures/ot/gen/41')]), u'Genesis 39; 41')
         self.assertEqual(format([ref('/scriptures/ot/gen/39'), ref('/scriptures/ot/gen/41'), ref('/scriptures/ot/gen/42')]), u'Genesis 39; 41–42')
+        self.assertEqual(format([ref('/scriptures/ot/ex/21-22')]), u'Exodus 21–22')
 
     def test_format_multiple_refs_across_books(self):
         self.assertEqual(format([ref('/scriptures/ot/gen/39.1'), ref('/scriptures/ot/ex/10.1-5')]), u'Genesis 39:1; Exodus 10:1–5')
