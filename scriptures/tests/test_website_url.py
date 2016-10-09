abba64d4fd2a405f368dcbd04208e9d67432fa3e
Hilton Campbell <hiltonc@ldsmobileapps.com>
2016-10-09 16:38:09 -0700

    Add lang to website_url

--- scriptures/tests/test_website_url.py
+++ scriptures/tests/test_website_url.py
@@ -8,6 +8,11 @@ class TestWebsiteURL(unittest.TestCase):
         website_url = 'https://www.lds.org/scriptures/ot/isa/40'
         self.assertEqual(scriptures.ref(uri).website_url(), website_url)
 
+    def test_no_verses_with_lang(self):
+        uri = '/scriptures/ot/isa/40'
+        website_url = 'https://www.lds.org/scriptures/ot/isa/40?lang=spa'
+        self.assertEqual(scriptures.ref(uri).website_url(lang='spa'), website_url)
+
     def test_single_verse_with_first(self):
         uri = '/scriptures/ot/isa/40.1'
         website_url = 'https://www.lds.org/scriptures/ot/isa/40.1#primary'
@@ -43,6 +48,11 @@ class TestWebsiteURL(unittest.TestCase):
         website_url = 'https://www.lds.org/scriptures/ot/isa/40.2#1'
         self.assertEqual(scriptures.ref(uri).website_url(), website_url)
 
+    def test_single_verse_with_lang(self):
+        uri = '/scriptures/ot/isa/40.2'
+        website_url = 'https://www.lds.org/scriptures/ot/isa/40.2?lang=spa#1'
+        self.assertEqual(scriptures.ref(uri).website_url(lang='spa'), website_url)
+
     def test_single_verse_range(self):
         uri = '/scriptures/ot/isa/40.2-4'
         website_url = 'https://www.lds.org/scriptures/ot/isa/40.2-4#1'
@@ -73,7 +83,17 @@ class TestWebsiteURL(unittest.TestCase):
         website_url = 'https://www.lds.org/scriptures/dc-testament/dc/76.56-57(50-70)#55'
         self.assertEqual(scriptures.ref(uri).website_url(), website_url)
 
+    def test_parentheticals_with_lang(self):
+        uri = '/scriptures/dc-testament/dc/76.56-57(50-70)'
+        website_url = 'https://www.lds.org/scriptures/dc-testament/dc/76.56-57(50-70)?lang=spa#55'
+        self.assertEqual(scriptures.ref(uri).website_url(lang='spa'), website_url)
+
     def test_chapter_ranges(self):
         uri = '/scriptures/bofm/alma/56-57'
         website_url = 'https://www.lds.org/scriptures/bofm/alma/56-57'
         self.assertEqual(scriptures.ref(uri).website_url(), website_url)
+
+    def test_chapter_ranges_with_lang(self):
+        uri = '/scriptures/bofm/alma/56-57'
+        website_url = 'https://www.lds.org/scriptures/bofm/alma/56-57?lang=spa'
+        self.assertEqual(scriptures.ref(uri).website_url(lang='spa'), website_url)
